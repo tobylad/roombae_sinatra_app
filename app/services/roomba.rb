9df5d4ee3c81e0e7c0023ff8553093d98175fdc2
majesty86 <tladislas@gmail.com>
2017-12-08 12:37:51 -0600

    Reassign GPIO pins, make it dance.

--- app/services/roomba.rb
+++ app/services/roomba.rb
@@ -34,20 +34,20 @@ module Roomba
   end
 
   def self.forward
+    self.prep_pin(7)
+    RPi::GPIO.set_high 7
+    sleep(3)
+    RPi::GPIO.set_low 7
+    puts self.on?(7)
+    self.reset
+  end
+
+  def self.right
     self.prep_pin(12)
     RPi::GPIO.set_high 12
     sleep(3)
     RPi::GPIO.set_low 12
     puts self.on?(12)
-    self.reset
-  end
-
-  def self.right
-    self.prep_pin(18)
-    RPi::GPIO.set_high 18
-    sleep(3)
-    RPi::GPIO.set_low 18
-    puts self.on?(18)
     self.reset
   end
 
@@ -60,26 +60,49 @@ module Roomba
     self.reset
   end
 
-  def self.pause
-    self.prep_pin(32)
-    RPi::GPIO.set_high 32
-    sleep(3)
-    RPi::GPIO.set_low 32
-    puts self.on?(32)
-    self.reset
-  end
+  # def self.pause
+  #   self.prep_pin(32)
+  #   RPi::GPIO.set_high 32
+  #   sleep(3)
+  #   RPi::GPIO.set_low 32
+  #   puts self.on?(32)
+  #   self.reset
+  # end
 
   def self.clean
-    self.prep_pin(num)
-    RPi::GPIO.set_high num
-    sleep(5)
-    RPi:GPIO.set_low num
-    puts self.on?(num)
+    self.prep_pin(37)
+    RPi::GPIO.set_high 37
+    sleep(10)
+    RPi:GPIO.set_low 37
+    puts self.on?(37)
     self.reset
   end
 
   def self.reset
     RPi::GPIO.clean_up    
+  end
+
+  def dance
+    self.forward
+    self.right
+    self.left
+    
+    4.times do 
+      self.right
+    end
+
+    2.times do 
+      self.left
+    end
+
+    self.forward
+
+    2.times do 
+      self.left
+    end
+
+    self.clean
+
   end
 
 end
